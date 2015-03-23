367af408885c88a86aa565e32f0bf1cb1d9f8972
Ansari90 <ansari90@outlook.com>
2015-03-22 21:49:24 -0400

    Final Commit
    Media file (image) not included!

---
+++ GoL_Driver.js
@@ -0,0 +1,177 @@
+﻿var tileEdge = 10; //each live tile should be 5 pixels across
+var worker, workerReady = 0;
+var genNumber = 0;
+var dataObject = new Object();
+var cellArray;
+
+//changing simulation meta setting results in a new simulation
+var currentMeta = new Object();
+currentMeta.gridEdge = -1;
+currentMeta.liveCells = -1;
+currentMeta.cellSpread = -1;
+
+//verifies if the entered data is valid
+function verifyInput() {
+    dataObject.newCanvas = 0;
+
+    var tmp1, tmp2, tmp3;
+    var problems = new Object;
+    problems.problemNum = 0;
+    problems.problemStatement = "Error! Improper Input: \n";
+
+
+    //we must ensure that a new Canvas is drawn even if one of the meta parameters remains unchanged
+    tmp1 = document.getElementById("gridEdge").value;
+    tmp2 = document.getElementById("liveCells").value;
+    tmp3 = document.getElementById("cellSpread").value;
+    
+    if (tmp1 < 3 || tmp1 == "" || isNaN(tmp1)) {
+        problems.problemNum++;
+        problems.problemStatement += "Grid too small for any meaningful simulation!\n";
+    }
+    else {
+        if (currentMeta.gridEdge != tmp1) {
+            dataObject.newCanvas = 1;
+            dataObject.gridEdge = tmp1;
+            dataObject.liveCells = tmp2;
+            dataObject.cellSpread = tmp3;
+
+            currentMeta.gridEdge = tmp1;
+        }
+    }
+
+    if (tmp2 <= 0 || tmp2 == "" || isNaN(tmp2)) {
+        problems.problemNum++;
+        problems.problemStatement += "Invalid initial seed value!\n";
+    }
+    else {
+        if (currentMeta.liveCells != tmp2) {
+            dataObject.newCanvas = 1;
+            dataObject.gridEdge = tmp1;
+            dataObject.liveCells = tmp2;
+            dataObject.cellSpread = tmp3;
+
+            currentMeta.liveCells = tmp2;
+        }
+    }
+
+    if (tmp3 <= 0 || tmp3 == "" || isNaN(tmp3)) {
+        problems.problemNum++;
+        problems.problemStatement += "Invalid initial seed value!\n";
+    }
+    else {
+        if (currentMeta.liveCells != tmp2) {
+            dataObject.newCanvas = 1;
+            dataObject.gridEdge = tmp1;
+            dataObject.liveCells = tmp2;
+            dataObject.cellSpread = tmp3;
+
+            currentMeta.liveCells = tmp3;
+        }
+    }
+
+    tmp1 = document.getElementById("lonelyDeath").value;
+    if (tmp1 < 0 || tmp1 > 8 || tmp1 == "" || isNaN(tmp1)) {
+        problems.problemNum++;
+        problems.problemStatement += "Invalid Underpopulation Parameter!\n";
+    }
+    else {
+        dataObject.lonelyDeath = tmp1;
+    }
+
+    tmp1 = document.getElementById("minLive").value;
+    tmp2 = document.getElementById("maxLive").value
+    if (tmp1 < 0 || tmp2 < 0 || (tmp1 > tmp2) || tmp1 == "" || isNaN(tmp1) || tmp2 == "" || isNaN(tmp2)) {
+        problems.problemNum++;
+        problems.problemStatement += "Invalid No Change Parameters!\n";
+    }
+    else {
+        dataObject.minLive = tmp1;
+        dataObject.maxLive = tmp2;
+    }
+
+    tmp1 = document.getElementById("crowdedDeath").value;
+    if (tmp1 < 0 || tmp1 > 8 || tmp1 == "" || isNaN(tmp1)) {
+        problems.problemNum++;
+        problems.problemStatement += "Invalid Overpopulation Parameter!\n";
+    }
+    else {
+        dataObject.crowdedDeath = tmp1;
+    }
+
+    tmp1 = document.getElementById("rezParam").value;
+    if (tmp1 < 0 || tmp1 > 8 || tmp1 == "" || isNaN(tmp1)) {
+        problems.problemNum++;
+        problems.problemStatement += "Invalid Resurrection Parameter!\n";
+    }
+    else {
+        dataObject.rezParam = tmp1;
+    }
+
+    return problems;
+}
+
+function startSimulation() {
+    var problems = verifyInput();
+
+    if (problems.problemNum > 0) {
+        alert(problems.problemStatement);
+    }
+    else {
+        if (dataObject.newCanvas == 1) {
+            genNumber = 0;
+        }
+
+        if (workerReady == 0) {
+            setupWorker();
+            dataObject.cellArrayPresent = 0;
+        }
+        else {
+            dataObject.cellArrayPresent = 1;
+            dataObject.cellArray = cellArray;
+
+            worker.terminate();
+            setupWorker();
+        }
+
+        worker.postMessage(dataObject);
+    }
+}
+
+function setupWorker() {
+    worker = new Worker("GoL_Worker.js");
+
+    worker.onmessage = function (event) {
+        cellArray = event.data;
+        genNumber++;
+        document.getElementById("displayArea").innerHTML = "";
+        document.getElementById("infoArea").innerHTML = "Generation #" + genNumber + "<br/>";
+
+        var theCanvas = document.createElement("canvas");
+        var verifiedEdge = Math.sqrt(cellArray.length);
+        var theTile = document.getElementById("liveTile");
+
+        theCanvas.setAttribute("width", (verifiedEdge * tileEdge) + tileEdge);
+        theCanvas.setAttribute("height", (verifiedEdge * tileEdge) + tileEdge);
+        document.getElementById("displayArea").appendChild(theCanvas);
+        var ctx = theCanvas.getContext("2d");
+
+        for (var row = 0; row < verifiedEdge; row++) {
+            for (var column = 0; column < verifiedEdge; column++) {
+                if (cellArray[(row * verifiedEdge) + column] == "alive") {
+                    ctx.drawImage(theTile, (column * tileEdge), (row * tileEdge));
+                }
+            }
+        }
+    };
+    workerReady = 1;
+}
+
+function stopSimulation() {
+    currentMeta.gridEdge = -1;
+    currentMeta.liveCells = -1;
+    document.getElementById("displayArea").innerHTML = "";
+
+    worker.terminate();
+    workerReady = 0;
+}
---
+++ GoL_Worker.js
@@ -0,0 +1,160 @@
+﻿var cellArray;
+var dataObject;
+
+self.onmessage = function (event) {
+    dataObject = event.data;
+    if (dataObject.cellArrayPresent == 1)
+        cellArray = dataObject.cellArray;
+
+    for (var i = 0; i < 100000; i++) {
+        for (var j = 0; j < 100000; j++) {      //a combined wait of 1 x 10^8 CPU cycles for my system (2.0 Ghz Core 2 Duo, Intel)
+            for (var k = 0; k < 1000; k++) { 
+                //
+            } 
+        }
+        if (dataObject.newCanvas == 1) {
+            setupGrid();
+            calculateInitialSeed();
+            dataObject.newCanvas = 0;
+        }
+        else {
+            spawnNextGeneration();
+        }
+
+        self.postMessage(cellArray);
+    }
+};
+
+function setupGrid() {
+    var gridEdge = dataObject.gridEdge;
+    cellArray = new Array();
+
+    for (var i = 0; i < (gridEdge * gridEdge); i++) {
+        cellArray[i] = "dead";
+    }
+}
+
+function calculateInitialSeed(l) {
+    var startingPoint = Math.floor(Math.random() * cellArray.length);
+    cellArray[startingPoint] = "alive";
+
+    var cellPoints = new Array();
+    cellPoints[0] = startingPoint;
+
+    var edge = Math.sqrt(cellArray.length), operation, random, randomPoint, i = 1;
+    while (i < dataObject.liveCells) {
+        random = Math.floor(Math.random() * dataObject.cellSpread);
+        operation = Math.floor(Math.random() * 7);
+
+        switch (operation) {
+            case 1:
+                randomPoint = startingPoint + random;
+                break;
+            case 2:
+                randomPoint = startingPoint - random;
+                break;
+            case 3:
+                randomPoint = startingPoint + edge + random;
+                break;
+            case 4:
+                randomPoint = startingPoint + edge - random;
+                break;
+            case 5:
+                randomPoint = startingPoint - edge + random;
+                break;
+            case 6:
+                randomPoint = startingPoint - edge - random;
+                break;
+            default:
+                //0 returned
+        }
+
+        if (randomPoint >= 0 && randomPoint < cellArray.length) {
+            if (cellPoints.indexOf(randomPoint) == -1) {
+                i++;
+                cellArray[randomPoint] = "alive";
+                startingPoint = randomPoint;
+                cellPoints[i - 1] = randomPoint;    //because i starts from 1 (1 cell has already been placed)
+            }
+        }
+    }
+}
+
+function spawnNextGeneration() {
+    var emergingGeneration = new Array();
+    var tempCellCounter, neighborCounter;
+    var edge = Math.sqrt(cellArray.length);
+
+    for (var cellCounter = 0; cellCounter < cellArray.length; cellCounter++) {
+        neighborCounter = 0;
+        
+        //middle cells
+        tempCellCounter = cellCounter - 1;
+        neighborCounter += neighborAlive(tempCellCounter);
+        
+        tempCellCounter = cellCounter + 1;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        //top cells
+        tempCellCounter = cellCounter - edge;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        tempCellCounter = cellCounter - edge - 1;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        tempCellCounter = cellCounter - edge + 1;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        //bottom cells
+        tempCellCounter = cellCounter + edge;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        tempCellCounter = cellCounter + edge - 1;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        tempCellCounter = cellCounter + edge + 1;
+        neighborCounter += neighborAlive(tempCellCounter);
+
+        if (neighborCounter < dataObject.lonelyDeath) {
+            emergingGeneration[cellCounter] = "dead";
+        } 
+        else {
+            if ((neighborCounter >= dataObject.minLive) && (neighborCounter <= dataObject.maxLive) && (cellArray[cellCounter] == "alive")) {
+                emergingGeneration[cellCounter] = "alive";
+            }
+            else {
+                if ((neighborCounter == dataObject.rezParam) && (cellArray[cellCounter] == "dead")) {
+                        emergingGeneration[cellCounter] = "alive";
+                } 
+                else {
+                    if (neighborCounter > dataObject.crowdedDeath)
+                        emergingGeneration[cellCounter] = "dead";
+                    else {
+                        emergingGeneration[cellCounter] = cellArray[cellCounter];
+                    }
+                }
+            }
+        }
+    }
+
+    cellArray = emergingGeneration;
+}
+
+function neighborAlive(cellNumber) {
+    var tempVar = validCellNumber(cellNumber);
+    if (tempVar == 1) {
+        if (cellArray[cellNumber] == "alive")
+            return 1;
+        else
+            return 0;
+    }
+    else
+        return 0;
+}
+
+function validCellNumber(number) {
+    if (number >= 0 && number < cellArray.length)
+        return 1;
+    else
+        return 0;
+}
